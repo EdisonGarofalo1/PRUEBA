118a5f536ae4ee2bea62729630a118ea00a2edfd
EdisonGarofalo1 <edisongarofalo88@gmail.com>
2024-04-12 08:50:06 +0100

    commit inicial

---
+++ src/main/java/aplicativo/prueba/AplicativoPruebaApplication.java
@@ -0,0 +1,15 @@
+package aplicativo.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoPruebaApplication.class, args);
+		
+		System.out.print("todo  bien el sistema");
+	}
+
+}
---
+++ src/test/java/aplicativo/prueba/AplicativoPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package aplicativo.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
